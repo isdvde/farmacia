6f088f702b6669ef23601981156e37b06a8d75ec
Adrian Moreno <adrianemoreno.25@gmail.com>
2021-10-25 18:03:01 -0400

    Adding Roles and Permissions

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -3,16 +3,55 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class PermissionTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+
+
+		/*Permission::create(['name' => 'empleado.*']);
+		Permission::create(['name' => 'farmacia.*']);
+		Permission::create(['name' => 'inventario.*']);
+		Permission::create(['name' => 'laboratorio.*']);
+		Permission::create(['name' => 'medicamento.*']);
+		Permission::create(['name' => 'pedido.*']);*/
+
+		$permission = [
+			'empleado',
+			'farmacia',
+			'inventario',
+			'laboratorio',
+			'medicamento',
+			'pedido',
+		];
+
+		$subPermission = [
+			'view',
+			'create',
+			'edit',
+			'delete',
+		];
+
+		foreach ($permission as $p) {
+			foreach ($subPermission as $sp) {
+				Permission::create(['name' => $p.'.'.$sp]);
+			}
+		}
+
+/*
+		Permission::findOrCreate('empleado.*');
+		Permission::findOrCreate('farmacia.*');
+		Permission::findOrCreate('inventario.*');
+		Permission::findOrCreate('laboratorio.*');
+		Permission::findOrCreate('medicamento.*');
+		Permission::findOrCreate('pedido.*');*/
+	}
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,29 +6,35 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Empleado;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-    	foreach (Empleado::all() as $empleado) {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		foreach (Empleado::all() as $empleado) {
 
-            for($i=1;; $i++) { 
-                $username = strtolower(substr($empleado->nombre, 0, $i).$empleado->apellido);
-                if (!(User::where('username',$username)->first())) {
-                    break;
-                }
-            }
-            User::factory()->create([
-             'username' => $username,
-             'password' => Hash::make($username),
-             'ci' => $empleado->ci,
-         ]);
-        }
-    }
+			for($i=1;; $i++) { 
+				$username = strtolower(substr($empleado->nombre, 0, $i).$empleado->apellido);
+				if (!(User::where('username',$username)->first())) {
+					break;
+				}
+			}
+
+			$user = User::factory()->create([
+				'username' => $username,
+				'password' => Hash::make($username),
+				'ci' => $empleado->ci,
+			]);
+
+			$user->assignRole($empleado->cargo);
+			$user->givePermissionTo(Role::findByName($empleado->cargo)->permissions()->pluck('name'));
+		}
+	}
 }
