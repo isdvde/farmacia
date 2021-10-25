85d39c041ffb99bd3faf979ec7591b62edf1c13c
Adrian Moreno <adrianemoreno.25@gmail.com>
2021-10-24 23:47:42 -0400

    Adding Roles and Permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,23 +11,28 @@ use App\Models\Laboratorio;
 use App\Models\Inventario;
 use App\Models\Pedido;
 use App\Models\PedidoMedicamento;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-        Farmacia::factory(15)->create();
-        Medicamento::factory(200)->create();
-        Laboratorio::factory(30)->create();
-        Inventario::factory(500)->create();
-        $this->call(EmpleadoSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(PedidoSeeder::class);
-    }
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// \App\Models\User::factory(10)->create();
+		Farmacia::factory(15)->create();
+		Medicamento::factory(200)->create();
+		Laboratorio::factory(30)->create();
+		Inventario::factory(200)->create();
+		$this->call(PermissionTableSeeder::class);
+		$this->call(RoleSeeder::class);
+		$this->call(EmpleadoSeeder::class);
+		$this->call(UserSeeder::class);
+		$this->call(PedidoSeeder::class);
+
+	}
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -8,57 +8,53 @@ use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-       $role1= Role::create(['name'=>'Admin']);
-       $role2= Role::create(['name'=>'Farmaceutico']);
-       $role3= Role::create(['name'=>'Auxiliar']);
-       $role4= Role::create(['name'=>'pasantes']);
-       $role5= Role::create(['name'=>'analista']);
-       $role6= Role::create(['name'=>'administrativo']);
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+/*		$admin = Role::create(['name' => 'admin']);
+		$farmaceutico = Role::create(['name' => 'farmaceutico']);
+		$analista = Role::create(['name' => 'analista']);
+		$pasante = Role::create(['name' => 'pasante']);*/
+
+		Role::create(['name' => 'admin'])->givePermissionTo(Permission::all());
+		Role::create(['name' => 'vigilante']);
+		Role::create(['name' => 'administrativo']);
+		Role::create(['name' => 'farmaceutico'])->givePermissionTo([
+			'inventario.view',
+			'inventario.create',
+			'inventario.edit',
+			'medicamento.create',
+			'medicamento.view',
+			'medicamento.edit',
+/*			'inventario,medicamento.*'*/
+		]);
+		Role::create(['name' => 'analista'])->givePermissionTo([
+				'inventario.view',
+				'inventario.create',
+				'inventario.edit',
+				'medicamento.create',
+				'medicamento.view',
+				'medicamento.edit',
+				'pedido.create',
+				'pedido.view',
+				'pedido.edit',
+				'laboratorio.create',
+				'laboratorio.view',
+				'laboratorio.edit',
+/*			'inventario,medicamento,pedido,laboratorio.*'*/
+		]);
+		Role::create(['name' => 'pasante'])->givePermissionTo([
+			'inventario.view',
+			'medicamento.view',
+
+/*			'inventario,medicamento.view'*/
+		]);
 
 
-        Permission::create(['name' => 'compra.index'])->syncRoles([$role1,$role2,$role5, $role4,$role6]);
-        Permission::create(['name' => 'compra.create'])->syncRoles([$role1,$role5,$role6]);
-        Permission::create(['name' => 'compra.edit'])->syncRoles([$role1,$role5,$role6]);
 
-
-        Permission::create(['name' => 'farmacia.index'])->syncRoles([$role1,$role2,$role3, $role5, $role4,$role6]);
-        Permission::create(['name' => 'farmacia.create'])->syncRoles([$role1,$role6]);
-        Permission::create(['name' => 'farmacia.edit'])->syncRoles([$role1,$role6]);
-        Permission::create(['name' => 'farmacia.delete'])->syncRoles([$role1,$role6]);
-
-
-        Permission::create(['name' => 'medicamento.index'])->syncRoles([$role1,$role5, $role4,$role6]);
-        Permission::create(['name' => 'medicamento.create'])->syncRoles([$role1,$role5,$role6]);
-        Permission::create(['name' => 'medicamento.edit'])->syncRoles([$role1,$role5,$role6]);
-        Permission::create(['name' => 'medicamento.delete'])->syncRoles([$role1,$role5,$role6]);
-
-
-        Permission::create(['name' => 'inventario.index'])->syncRoles([$role1,$role2,$role3,$role5, $role4,$role6]);
-        Permission::create(['name' => 'inventario.create'])->syncRoles([$role1,$role5,$role6]);
-
-
-        Permission::create(['name' => 'laboratorio.index'])->syncRoles([$role1,$role2,$role3,$role5, $role4,$role6]);
-        Permission::create(['name' => 'laboratorio.create'])->syncRoles([$role1,$role5,$role6]);
-        Permission::create(['name' => 'laboratorio.edit'])->syncRoles([$role1,$role5,$role6]);
-        Permission::create(['name' => 'laboratorio.delete'])->syncRoles([$role1,$role5,$role6]);
-
-
-        Permission::create(['name' => 'pedido.index'])->syncRoles([$role1,$role2,$role3,$role5, $role4,$role6]);
-        Permission::create(['name' => 'pedido.create'])->syncRoles([$role1,$role5,$role6]);
-        Permission::create(['name' => 'pedido.edit'])->syncRoles([$role1,$role5,$role6]);
-        Permission::create(['name' => 'pedido.delete'])->syncRoles([$role1,$role5,$role6]);
-
-
-        Permission::create(['name' => 'empleado.index'])->syncRoles([$role1,$role2,$role6]);
-        Permission::create(['name' => 'empleado.create'])->syncRoles([$role1,$role6]);
-        Permission::create(['name' => 'empleado.edit'])->syncRoles([$role1,$role6]);
-        Permission::create(['name' => 'empleado.delete'])->syncRoles([$role1,$role6]);
-    }
+	}
 }
